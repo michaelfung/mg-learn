52a2257c2f8c839cebef945526dd2ed470c752a0
Michael Fung @ Acer <hkuser2001@gmail.com>
2017-07-02 15:01:29 +0800

    sonoff basic works with openhab2 remove skylink emulation code in fw

--- esp/openhab/fs/init.js
+++ esp/openhab/fs/init.js
@@ -19,16 +19,11 @@ let thing_id = Cfg.get('mqtt.client_id');
 let hab_control_topic = 'hab2/switch/' + thing_id;
 let hab_state_topic = 'hab2/switch/' + thing_id + '/state';
 let led_onboard = 13; // Sonoff LED pin
-let sw_pin = 12;  // Sonoff relay pin
+let relay_pin = 12;  // Sonoff relay pin
 let spare_pin = 14;  // Sonoff not connected
 let button_pin = 0;  // Sonoff push button
-let sw_state = {
-    dev_id: '1',
-    value: 0,
-    status: 'OFF',
-    battery: 1,
-    time: 0
-};
+let relay_state = 'OFF';
+let relay_value = 0;
 let last_toggle = 0;
 let tick_count = 0;
 
@@ -36,28 +31,18 @@ let tick_count = 0;
 GPIO.set_mode(led_onboard, GPIO.MODE_OUTPUT);
 GPIO.write(led_onboard, 1);  // default to off
 
-GPIO.set_mode(sw_pin, GPIO.MODE_OUTPUT);
-GPIO.write(sw_pin, 0);  // default to off
+GPIO.set_mode(relay_pin, GPIO.MODE_OUTPUT);
+GPIO.write(relay_pin, 0);  // default to off
 
 GPIO.set_mode(spare_pin, GPIO.MODE_INPUT);
 GPIO.set_mode(button_pin, GPIO.MODE_INPUT);
 
-// define functions:
-let get_sw_state = function() {
-    sw_state.time = Timer.now();
-    sw_state.status = sw_state.value ? 'ON' : 'OFF';
-    return sw_state;
-};
-
 /* notify server of switch state */
 let update_state = function() {
     let pubmsg = JSON.stringify({
-        op: 'state',
         uptime: Sys.uptime(),
         memory: Sys.free_ram(),
-        data: [
-            get_sw_state()
-        ]
+        relay_state: relay_state
     });
     let ok = MQTT.pub(hab_state_topic, pubmsg);
     Log.print(Log.INFO, 'Published:' + (ok ? 'OK' : 'FAIL') + ' topic:' + hab_state_topic + ' msg:' +  pubmsg);
@@ -66,11 +51,11 @@ let update_state = function() {
 /* toggle switch with bounce protection */
 let toggle_switch = function() {
     if ( (Sys.uptime() - last_toggle ) > 2 ) {
-        GPIO.toggle(sw_pin);
-        sw_state.value = 1 - sw_state.value; // 0 1 toggle
+        GPIO.toggle(relay_pin);
+        relay_value = 1 - relay_value; // 0 1 toggle
         last_toggle = Sys.uptime();
     } else {
-        Log.print(Log.INFO, 'Too frequent. Toggle not allowed.');
+        Log.print(Log.INFO, 'Bounce protection: toggle not allowed.');
     }
 };
 
@@ -83,46 +68,37 @@ GPIO.set_button_handler(button_pin, GPIO.PULL_UP, GPIO.INT_EDGE_NEG, 500, functi
     update_state();
 }, true);
 
-MQTT.sub(hab_control_topic, function(conn, topic, msg) {
-    Log.print(Log.INFO, 'rcvd ctrl msg:' + msg);
-    let parsed = JSON.parse(msg);
+MQTT.sub(hab_control_topic, function(conn, topic, command) {
+    Log.print(Log.INFO, 'rcvd ctrl msg:' + command);
 
-    if ( parsed.op === 'set' ) {
-        if ( parsed.value === 1 ) {
+
+        if ( command === 'ON' ) {
             if ( (Sys.uptime() - last_toggle ) > 2 ) {
-                GPIO.write(sw_pin, 1);  // set switch to on
-                sw_state.value = 1;
+                GPIO.write(relay_pin, 1);  // set switch to on
+                relay_state.value = 1;
                 last_toggle = Sys.uptime();
             }
-        } else if ( parsed.value === 0 ) {
-                   GPIO.write(sw_pin, 0);  // set switch to off
-                   sw_state.value = 0;
+        } else if ( command === 'OFF' ) {
+                   GPIO.write(relay_pin, 0);  // set switch to off
+                   relay_state.value = 0;
                    last_toggle = Sys.uptime();
         } else {
-                //parsed.errno = 5;  /* 5: invalid param */
-                Log.print(Log.ERROR, 'Unsupported value');
+			Log.print(Log.ERROR, 'Unsupported command');
         }
-    }
-
-    else {
-        Log.print(Log.ERROR, 'Unsupported Operation');
-    }
 
     update_state();
 
 }, null);
 
 
-// Blink built-in LED every second
+// timer loop to update state
 Timer.set(1000 /* 1 sec */, true /* repeat */, function() {
 
-  let value = GPIO.toggle(led_onboard);
-  print(value ? 'Tick' : 'Tock', 'uptime:', Sys.uptime());
   tick_count++;
-  tick_count = tick_count % 60;
+  tick_count = tick_count % 300;
   if ( tick_count === 0 ) {
 		update_state();
   }
 }, null);
 
-Log.print(Log.WARN, "### init script started ###");
+Log.print(Log.WARN, "### BOOT UP Script Started ###");
--- esp/openhab/src/main.c
+++ esp/openhab/src/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <time.h>
 
 #include "common/platform.h"
 #include "common/cs_file.h"
@@ -8,28 +9,72 @@
 #include "fw/src/mgos_timers.h"
 #include "fw/src/mgos_hal.h"
 #include "fw/src/mgos_dlsym.h"
+#include "mgos_mqtt.h"
 #include "mjs.h"
 
-#if CS_PLATFORM == CS_P_ESP8266
-#define LED_GPIO 2 /* On ESP-12E there is a blue LED connected to GPIO2  */
-#elif CS_PLATFORM == CS_P_ESP32
-#define LED_GPIO 21 /* No LED on DevKitC, use random GPIO close to GND pin */
-#elif CS_PLATFORM == CS_P_CC3200
-#define LED_GPIO 64 /* The red LED on LAUNCHXL */
-#elif(CS_PLATFORM == CS_P_STM32) && defined(BSP_NUCLEO_F746ZG)
-/* Nucleo-144 F746 */
-#define LED_GPIO STM32_PIN_PB7 /* Blue LED */
-#elif(CS_PLATFORM == CS_P_STM32) && defined(BSP_DISCO_F746G)
-/* Discovery-0 F746 */
-#define LED_GPIO STM32_PIN_PI1 /* Green LED */
-#else
-#error Unknown platform
-#endif
+static int ON_BOARD_LED = 13; /* sonoff basic LED pin */
+
+bool mqtt_conn_flag = false;
+static uint8_t led_timer_count = 0;  /* for led blinker use */
 
 int get_led_gpio_pin(void) {
-  return LED_GPIO;
+  return ON_BOARD_LED;
+}
+
+int mqtt_connected(void) {
+	return (int) mqtt_conn_flag;
+}
+
+static void blink_on_board_led_cb(void *arg) {
+    static uint8_t remainder;
+
+    if (mqtt_conn_flag) {
+        remainder = (++led_timer_count % 40);
+        if (remainder == 0) {
+            led_timer_count = 0;
+            mgos_gpio_write(ON_BOARD_LED, 0);  // on
+        } else if (remainder == 1) {
+            mgos_gpio_write(ON_BOARD_LED, 1);  // off
+        }
+    } else {
+        mgos_gpio_toggle(ON_BOARD_LED);
+    }
+    (void) arg;
+}
+
+static void pub(struct mg_connection *c, const char *fmt, ...) {
+  char msg[200];
+  struct json_out jmo = JSON_OUT_BUF(msg, sizeof(msg));
+  va_list ap;
+  int n;
+  va_start(ap, fmt);
+  n = json_vprintf(&jmo, fmt, ap);
+  va_end(ap);
+  mg_mqtt_publish(c, get_cfg()->mqtt.pub, 0, MG_MQTT_QOS(0), msg, n);
+  LOG(LL_INFO, ("%s -> %s", get_cfg()->mqtt.pub, msg));
+}
+
+static void mqtt_ev_handler(struct mg_connection *c, int ev, void *p, void *user_data) {
+  struct mg_mqtt_message *msg = (struct mg_mqtt_message *) p;
+  if (ev == MG_EV_MQTT_CONNACK) {
+    LOG(LL_INFO, ("CONNACK: %d", msg->connack_ret_code));
+    mqtt_conn_flag = true;
+    if (get_cfg()->mqtt.pub == NULL) {
+      LOG(LL_ERROR, ("Run 'mos config-set mqtt.pub=... '"));
+    } else {
+      pub(c, "{timestamp:%.3lf, mem_free:%d}", mg_time(), mgos_get_free_heap_size() );  /* post uptime */
+    }
+  } else if (ev == MG_EV_CLOSE) {
+      mqtt_conn_flag = false;
+  }
+  (void) user_data;
 }
 
 enum mgos_app_init_result mgos_app_init(void) {
+
+  mgos_gpio_set_mode(ON_BOARD_LED, MGOS_GPIO_MODE_OUTPUT);
+  mgos_set_timer(100 /* ms */, true /* repeat */, blink_on_board_led_cb, NULL);
+
+  mgos_mqtt_add_global_handler(mqtt_ev_handler, NULL);
   return MGOS_APP_INIT_SUCCESS;
 }
