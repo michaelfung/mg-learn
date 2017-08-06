a32bc32d40d96aada852a89e6b64b8259b455cc1
Michael Fung <hkuser2001@gmail.com>
2017-08-06 18:14:47 +0800

    init hmac basic structures

--- esp/hmac/hmac/hmac.c
+++ esp/hmac/hmac/hmac.c
@@ -1,8 +1,40 @@
 #include <stdio.h>
 #include <ctype.h>
+#include "common/platform.h"
 #include "mgos_dlsym.h"
 #include "mbedtls/md.h"
 #include "hmac.h"
+
+// data
+static mbedtls_md_context_t *ctx;
+const mbedtls_md_info_t *md_info;
+static unsigned char key[16];
+unsigned char digest[20];
+
+// helper functions
+static uint8_t hexdec(const char *s) {
+#define HEXTOI(x) (x >= '0' && x <= '9' ? x - '0' : x - 'W')
+  int a = tolower(*(const unsigned char *) s);
+  int b = tolower(*(const unsigned char *) (s + 1));
+  return (HEXTOI(a) << 4) | HEXTOI(b);
+}
+
+/*
+ * load key from a hexstring
+ * must be 32 chars or more
+ */
+int loadkey(char *hexstring) {
+	char *hexstrptr = hexstring;
+	if (sizeof(hexstring) < 32) {
+		return 1;
+	}
+	int i = 0;
+	for (;  i < 16; i++) {
+		key[i] = hexdec(hexstrptr);
+		hexstrptr += 2;
+	}
+	return 0;
+}
 
 void tohex(unsigned char * in, size_t insz, char * out, size_t outsz) {
     unsigned char *pin = in;
@@ -18,3 +50,10 @@ void tohex(unsigned char * in, size_t insz, char * out, size_t outsz) {
     pout[0] = 0;
 }
 
+
+// ffi'able functions
+int hmac_init() {
+ md_info = mbedtls_md_info_from_type( MBEDTLS_MD_SHA1 );
+ mbedtls_md_init( ctx );
+ return mbedtls_md_setup( ctx, md_info, 1 );  // param 'hmac' set to 1
+}
--- esp/hmac/hmac/hmac.h
+++ esp/hmac/hmac/hmac.h
@@ -1,6 +1,7 @@
 #ifndef __HMAC_H
 #define __HMAC_H
 
+int loadkey(char *hexstring);
 void tohex(unsigned char * in, size_t insz, char * out, size_t outsz);
 
 #endif
